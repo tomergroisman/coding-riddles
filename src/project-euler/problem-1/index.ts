7b7664a7e23a44c283d0ca509d4a90b0797214bb
Tomer Groisman <tomergroisman@gmail.com>
2021-12-31 15:25:46 +0200

    Remove comments

--- src/project-euler/problem-1/index.ts
+++ src/project-euler/problem-1/index.ts
@@ -21,12 +21,4 @@ export function multiplesOfThreeOrFive(n: number) {
   const duplicationsArithmeticSequenceSum = 15 * arithmeticSequenceSum(numberOfDuplications);
 
   return threesArithmeticSequenceSum + fivesArithmeticSequenceSum - duplicationsArithmeticSequenceSum;
-
-  // let sum = 0;
-  // for (let i = 0; i < n; i++) {
-  //   if (i % 3 == 0 || i % 5 == 0) {
-  //     sum += i;
-  //   }
-  // }
-  // return sum;
 }
